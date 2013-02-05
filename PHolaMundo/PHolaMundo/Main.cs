53f15e82daa946c92f006e1127a0fbb69b66d192
blanca <blanca1.dam@gmail.com>
2013-02-05 17:28:36 +0100

    Actualizando mi nuevo repositorio

---
+++ PHolaMundo/PHolaMundo/Main.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace PHolaMundo
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			showMessage ();
+			showMessage2 ();
+		}
+		
+		private static void showMessage()
+		{
+			Console.WriteLine ("Prueba de git con Hola Mundo");
+		}
+		
+		private static void showMessage2()
+		{
+			Console.WriteLine ("Hello Word!");
+		}
+	}
+}
